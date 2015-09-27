a2e0159098a74ceb853860a234ad46f49be872c3
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-09-27 11:22:17 +0900

    add endpoint

--- config/config.exs
+++ config/config.exs
@@ -23,7 +23,8 @@ use Mix.Config
 #
 
 config :my_bot_ex,
-  token: "431a9ce93c6a55ba024a63ac5c82e5786c14b437"
+  token: "431a9ce93c6a55ba024a63ac5c82e5786c14b437",
+  endpoint: "https://api.github.com"
 
 
 
