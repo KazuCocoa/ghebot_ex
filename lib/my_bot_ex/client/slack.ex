c01f3c8ed0f8ef22cdf9f5b49591365690c26468
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-10-12 00:36:00 +0900

    comment out some sending message

--- lib/my_bot_ex/client/slack.ex
+++ lib/my_bot_ex/client/slack.ex
@@ -5,7 +5,7 @@ defmodule MyBotEx.Client.Slack do
   # https://github.com/BlakeWilliams/Elixir-Slack/blob/63eba17c53f68a99abd32ceb1c5d526065b02319/lib/slack.ex#L45
 
   def handle_connect(slack, state) do
-    send_message("hello", "C02AT3GEG", slack)
+    # send_message("hello", "C02AT3GEG", slack)
     IO.puts "Connected as #{slack.me.name}"
     {:ok, state}
   end
@@ -13,7 +13,7 @@ defmodule MyBotEx.Client.Slack do
   def handle_message(message = %{type: "message"}, slack, state) do
     message_to_send = "Received #{length(state)} messages so far!"
     IO.puts  message.channel
-    send_message(message_to_send, message.channel, slack)
+    # send_message(message_to_send, message.channel, slack)
 
     {:ok, state ++ [message.text]}
   end
