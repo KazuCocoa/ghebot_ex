30cf3b6a4b9560332cb59e4f5e67af03490686bc
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-10-18 16:27:29 +0900

    add configs for each environments

--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,5 @@
 use Mix.Config
 
-config :my_bot_ex,
-  token: "431a9ce93c6a55ba024a63ac5c82e5786c14b437",
-  endpoint: "https://api.github.com",
-  api_key: "xoxb-12298332852-SXjvQu4F8j6ruMU6s79Ih0mb" # my sample bot
-
 config :reporter,
   droid_package: "com.android.chrome",
   droid_locale: "en",
@@ -12,3 +7,5 @@ config :reporter,
   app_id: "375380948",
   app_locale: "en",
   get_count_apple: 5
+
+import_config "#{Mix.env}.exs"
