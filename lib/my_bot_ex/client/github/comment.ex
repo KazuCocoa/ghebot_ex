588a4efd0eb29a3865a82f67c94b29e9f8cf6529
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-10-22 23:30:14 +0900

    add comment and event

---
+++ lib/my_bot_ex/client/github/comment.ex
@@ -0,0 +1,11 @@
+defmodule MyBotEx.Client.Github.Comment do
+  alias Tentacat, as: TC
+
+  def issue(client, owner, repo, issue_number, options \\ []) do
+    TC.get "repos/#{owner}/#{repo}/issues/#{issue_number}/comments", client
+  end
+
+  def issues(client, owner, repo, options \\ []) do
+    TC.get "/repos/#{owner}/#{repo}/issues/comments", client
+  end
+end
