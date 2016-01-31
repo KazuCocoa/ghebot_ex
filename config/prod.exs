2e8cc434494c35e582ffacfe11dba63fa4207b8c
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-01-31 14:16:18 +0900

    use system env

--- config/prod.exs
+++ config/prod.exs
@@ -3,4 +3,4 @@ use Mix.Config
 config :my_bot_ex,
   token: "431a9ce93c6a55ba024a63ac5c82e5786c14b437",
   endpoint: "https://api.github.com",
-  api_key: "xoxb-12298332852-SXjvQu4F8j6ruMU6s79Ih0mb" # my sample bot
+  api_key: System.get_env("BOT_TOKEN")
