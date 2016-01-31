b1e1b788e94d950c353e9d9b86e630145c87a0e3
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-01-31 21:29:21 +0900

    add revision and port

--- config/prod.exs
+++ config/prod.exs
@@ -4,3 +4,6 @@ config :my_bot_ex,
   token: "431a9ce93c6a55ba024a63ac5c82e5786c14b437",
   endpoint: "https://api.github.com",
   api_key: System.get_env("BOT_TOKEN")
+
+config :revision_plate_ex,
+  http_port: {:system, "PORT"},
