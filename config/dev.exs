e352ba6d617c3dfcbb6dcafd166a2f711de7ecab
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-03-15 07:13:10 +0900

    update trend

--- config/dev.exs
+++ config/dev.exs
@@ -3,4 +3,4 @@ use Mix.Config
 config :my_bot_ex,
   token: "8faa80ed8d219c02a52b2c5a376929d5c5562701",
   endpoint: "https://api.github.com",
-  api_key: "xoxb-12298332852-SXjvQu4F8j6ruMU6s79Ih0mb" # my sample bot
+  api_key: "xoxb-12298332852-wZhlAGTIW3HFlSecU7bJXKKj" # my sample bot
