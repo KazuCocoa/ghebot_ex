b669b03e17e9a556ac660d1fb04ee38a236f5e42
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-04-17 21:55:41 +0900

    set port as integer

--- lib/my_bot_ex/supervisor.ex
+++ lib/my_bot_ex/supervisor.ex
@@ -11,10 +11,13 @@ defmodule MyBotEx.Supervisor do
 
     api_key = Application.get_env :my_bot_ex, :api_key
 
+    port = System.get_env "PORT"
+    if !is_integer(port), do: port = String.to_integer(port)
+
     children = [
       worker(Slack, [api_key, [name: Slack]]),
       worker(Github, [[name: Github]]),
-      worker(Router, [[port: System.get_env("PORT")]])
+      worker(Router, [[port: port]])
     ]
 
     supervise children, strategy: :one_for_one
